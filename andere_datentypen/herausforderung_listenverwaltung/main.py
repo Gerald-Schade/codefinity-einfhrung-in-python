848bb0405ac49a3e9edef5172087b2160c4e7243
Gerald-Schade <>
2025-12-22 17:30:43 +0000

    Herausforderung: Listenverwaltung

---
+++ andere_datentypen/herausforderung_listenverwaltung/main.py
@@ -0,0 +1,32 @@
+#Listen 
+meat = ["Ham", 3.99, 50, "Sliced"]
+cheese = ["Cheddar", 5.49, 100, "Sharp"]
+condiment = ["Mustard", 1.99, 75, "Spicy"]
+
+
+    
+#Hauptliste aus allen listen
+deli_dept = [meat, cheese, condiment]
+
+print("Initial Deli List:", deli_dept)
+
+#Nachfüllen 
+if "Ham" in meat and meat[2] < 100:
+    meat[2] = 100
+
+#anhängen einer neuen Liste
+seasonal_meat = ["Turkey", 4.50, 100, "Sliced"]
+deli_dept.append(seasonal_meat)
+deli_dept.remove(condiment)
+deli_dept.sort()
+print("Updated Deli List:", deli_dept)
+
+
+
+
+
+
+
+
+
+
