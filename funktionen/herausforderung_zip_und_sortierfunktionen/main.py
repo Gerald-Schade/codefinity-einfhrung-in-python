2533e6721249d2b605b8f6d6d731033a8cda9753
Gerald-Schade <>
2025-12-28 18:20:52 +0000

    Herausforderung: Zip- und Sortierfunktionen

---
+++ funktionen/herausforderung_zip_und_sortierfunktionen/main.py
@@ -0,0 +1,14 @@
+# List of product names
+products = ["Banana", "Apple", "Mango", "Cherry"]
+
+# List of product prices
+prices = [1.20, 0.50, 2.50, 1.75]
+
+# List of quantity sold
+quantities_sold = [50, 100, 25, 40]
+
+#Listen zuzsammenführen
+combined_list = list(zip(products, prices, quantities_sold))
+'Listen aufsteigend sortieren:
+
+print(combined_list)
